09d1a2eca5cee40eac8c692620349f718b59a3e6
АланиКзереб <ale.berezckina@yandex.ru>
2022-10-31 23:57:48 +0300

    edited after comments (post /offers and pep8)

--- app/utils.py
+++ app/utils.py
@@ -1,10 +1,8 @@
 import json
 
 from flask import Flask
-
 from app import db
-from app.Models import Offer, Order, User
-
+from app.models import Offer, Order, User
 
 
 def load_data(path):
@@ -16,20 +14,17 @@ def load_data(path):
 
 def load_offers(path):
     offers = load_data(path)
-
     for offer in offers:
         db.session.add(
             Offer(
                 **offer
                 )
             )
-
         db.session.commit()
 
 
 def load_orders(path):
     orders = load_data(path)
-
     for order in orders:
         db.session.add(
             Order(
@@ -41,7 +36,6 @@ def load_orders(path):
 
 def load_users(path):
     users = load_data(path)
-
     for user in users:
         db.session.add(
             User(
@@ -54,21 +48,18 @@ def load_users(path):
 def create_app():
     """создание подключения"""
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///my.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['RESTX_JSON'] = {'ensure_ascii': False, 'indent': 4}
     app.config['SQLALCHEMY_EHO'] = True
-
     with app.app_context():
         db.init_app(app)
         db.drop_all()
         db.create_all()
-
         db.create_all()
         offer = load_offers('/Users/User/PycharmProjects/hw_16_parf/data/offers.json')
         order = load_orders('/Users/User/PycharmProjects/hw_16_parf/data/orders.json')
         user = load_users('/Users/User/PycharmProjects/hw_16_parf/data/users.json')
-
     return app
 
 app = create_app()
--- app/vews.py
+++ app/vews.py
@@ -1,9 +1,7 @@
 import json
 
 from flask import request
-
-
-from app.Models import User, Order, Offer
+from app.models import User, Order, Offer
 from app.utils import db, app
 
 
@@ -12,22 +10,17 @@ def get_users():
     if request.method == 'GET':
         result = []
         for user in db.session.query(User).all():
-
             result.append(user.get_data())
-
         return app.response_class(json.dumps(result),
                                   mimetype='application/json',
                                   status=200)
-
     if request.method == 'POST':
-
         data = request.json
         db.session.add(
             User(
                 **data
             )
         )
-
         return app.response_class(json.dumps("ok"),
                                   mimetype='application/json',
                                   status=200)
@@ -38,19 +31,15 @@ def get_user_by_id(uid):
     if request.method == 'GET':
         result = []
         for user in db.session.query(User).filter(User.id == uid).all():
-
             result.append(user.get_data())
-
         return app.response_class(json.dumps(result),
                                   mimetype='application/json',
                                   status=200)
-
-
     if request.method == 'PUT':
-
         data = request.json
         try:
-            user = db.session.filter(User.id == uid).get(uid)
+            user = db.session.query(User).get(uid)
+            # user = db.session.query(User).filter(User.id == uid).get(uid)
             user.id = data.get("id")
             user.first_name = data.get("first_name")
             user.last_name = data.get("last_name")
@@ -58,194 +47,138 @@ def get_user_by_id(uid):
             user.email = data.get("email")
             user.role = data.get("role")
             user.phone = data.get("phone")
-
             db.session.commit()
-
         except Exception as e:
             print(e)
-
-
-
         return app.response_class(json.dumps("ok"),
                                   mimetype='application/json',
                                   status=200)
-    if request.method == 'DELETE':
-
+    elif request.method == 'DELETE':
         try:
             db.session.query(User).filter(User.id == uid).delete()
             db.session.commit()
-
         except Exception as e:
             print(e)
-
-
-
         return app.response_class(json.dumps("ok"),
                                   mimetype='application/json',
                                   status=200)
-
 
 
 @app.route("/orders", methods=['GET', 'POST'])
 def get_orders():
     if request.method == 'GET':
         result = []
-        for user in db.session.query(Order).all():
-
-            result.append(user.get_data())
-
+        for order in db.session.query(Order).all():
+            result.append(order.get_data())
         return app.response_class(json.dumps(result),
                                   mimetype='application/json',
                                   status=200)
-
     if request.method == 'POST':
-
         data = request.json
         db.session.add(
-            User(
+            Order(
                 **data
             )
         )
-
         return app.response_class(json.dumps("ok"),
                                   mimetype='application/json',
                                   status=200)
+
 
 @app.route("/orders/<int:oid>", methods=['GET', 'PUT', 'DELETE'])
 def get_orders_by_id(oid):
     if request.method == 'GET':
         result = []
-        for user in db.session.query(Order).filter(Order.id == oid).all():
-
-            result.append(user.get_data())
-
+        for order in db.session.query(Order).filter(Order.id == oid).all():
+            result.append(order.get_data())
         return app.response_class(json.dumps(result),
                                   mimetype='application/json',
                                   status=200)
-
-
     if request.method == 'PUT':
-
         data = request.json
         try:
             order = db.session.filter(Order.id == oid).get(oid)
             order.id = data.get("id")
-            order.name = data.get("first_name")
-            order.description = data.get("last_name")
-            order.start_date = data.get("age")
-            order.end_date = data.get("email")
-            order.address = data.get("role")
-            order.price = data.get("phone")
+            order.name = data.get("name")
+            order.description = data.get("description")
+            order.start_date = data.get("start_date")
+            order.end_date = data.get("end_date")
+            order.address = data.get("address")
+            order.price = data.get("price")
             order.customer_id = data.get("customer_id")
             order.executor_id = data.get("executor_id")
-
             db.session.commit()
-
         except Exception as e:
             print(e)
-
-
-
         return app.response_class(json.dumps("ok"),
                                   mimetype='application/json',
                                   status=200)
     if request.method == 'DELETE':
-
         try:
-            db.session.query(Order).filter(User.id == oid).delete()
+            db.session.query(Order).filter(Order.id == oid).delete()
             db.session.commit()
-
         except Exception as e:
             print(e)
-
-
 
         return app.response_class(json.dumps("ok"),
                                   mimetype='application/json',
                                   status=200)
-
 
 
 @app.route("/offers", methods=['GET', 'POST'])
 def get_offers():
     if request.method == 'GET':
         result = []
-        for user in db.session.query(Offer).all():
-
-            result.append(user.get_data())
-
+        for offer in db.session.query(Offer).all():
+            result.append(offer.get_data())
         return app.response_class(json.dumps(result),
                                   mimetype='application/json',
                                   status=200)
-
     if request.method == 'POST':
-
         data = request.json
         db.session.add(
-            User(
+            Offer(
                 **data
             )
         )
-
         return app.response_class(json.dumps("ok"),
                                   mimetype='application/json',
                                   status=200)
+
 
 @app.route("/offers/<int:oid>", methods=['GET', 'PUT'])
 def get_offers_by_id(oid):
     if request.method == 'GET':
         result = []
-        for user in db.session.query(Offer).filter(Offer.id == oid).all():
-
-            result.append(user.get_data())
-
+        for offer in db.session.query(Offer).filter(Offer.id == oid).all():
+            result.append(offer.get_data())
         return app.response_class(json.dumps(result, ensure_ascii=False),
                                   mimetype='application/json', status=200)
-
     if request.method == 'PUT':
-
         data = request.json
         try:
-            order = db.session.filter(Order.id == oid).get(oid)
-            order.id = data.get("id")
-            order.name = data.get("first_name")
-            order.description = data.get("last_name")
-            order.start_date = data.get("age")
-            order.end_date = data.get("email")
-            order.address = data.get("role")
-            order.price = data.get("phone")
-            order.customer_id = data.get("customer_id")
-            order.executor_id = data.get("executor_id")
-
+            offer = db.session.filter(Offer.id == oid).get(oid)
+            offer.id = data.get("id")
+            offer.order_id = data.get("order_id")
+            offer.executor_id = data.get("executor_id")
             db.session.commit()
-
         except Exception as e:
             print(e)
-
-
-
         return app.response_class(json.dumps("ok"),
                                   mimetype='application/json',
                                   status=200)
-
     if request.method == 'DELETE':
-
         try:
-            db.session.query(Order).filter(User.id == oid).delete()
+            db.session.query(Offer).filter(Offer.id == oid).delete()
             db.session.commit()
-
         except Exception as e:
             print(e)
-
-
-
         return app.response_class(json.dumps("ok"),
                                   mimetype='application/json',
                                   status=200)
 
 
 if __name__ == '__main__':
-
     app.run()
 
 
